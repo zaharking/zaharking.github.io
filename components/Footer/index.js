c1d3087f92063e0a1fd662810537676760a310ab
zaharking <zahmoki@gmail.com>
2025-03-31 17:00:38 -0700

    links cleanup

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -16,19 +16,12 @@ const Footer = ({}) => {
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
               TOGETHER
             </h1>
-            <Button type="primary">Schedule a call</Button>
             <div className="mt-10">
               <Socials />
             </div>
           </div>
         </div>
       </div>
-      <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        Made With ❤ by{" "}
-        <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
-        </Link>
-      </h1>
     </>
   );
 };
